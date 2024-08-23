d0c932b4cf3e024bbddb22db65cd82f5bead8a15
Atsubyo <art.tyoung@tamu.edu>
2024-08-22 21:07:57 -0500

    Added column values

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+
+import React, { useMemo } from "react";
+import { AgGridReact } from "ag-grid-react";
+import { Container } from "@mantine/core";
 
 const Dashboard = () => {
-	return <div>Dashboard</div>;
+	const colDef = useMemo(
+		() => [
+			{ field: "Name" },
+			{ field: "Email" },
+			{ field: "Phone Number" },
+			{ field: "Reimbursement Amount" },
+			{ field: "Time Requested" },
+		],
+		[]
+	);
+
+	return (
+		<Container>
+			<AgGridReact columnDefs={colDef} />
+		</Container>
+	);
 };
 
 export default Dashboard;
