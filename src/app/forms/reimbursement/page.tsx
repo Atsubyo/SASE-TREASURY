5f84e3fa493673e0b4b718da7145fc9c2dd71db6
Atsubyo <art.tyoung@tamu.edu>
2024-09-11 18:28:02 -0500

    Added form state manager and validation

--- src/app/forms/reimbursement/page.tsx
+++ src/app/forms/reimbursement/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import React, { useState } from "react";
+import Reacts from "react";
 import {
+	Button,
 	Center,
 	Container,
 	FileInput,
@@ -15,35 +16,48 @@ import {
 	TextInput,
 } from "@mantine/core";
 import { DatePickerInput } from "@mantine/dates";
+import { useForm, zodResolver } from "@mantine/form";
 import { IconCalendar, IconFileTypePdf } from "@tabler/icons-react";
-import styles from "../forms.module.css";
+import styles from "@/app/forms/forms.module.css";
 import { SASE_BLUE } from "@/types/StyleConstants";
-import { ReimbursementFormType } from "@/types/FormTypes";
+import {
+	ReimbursementFormType,
+	ReimbursementFormSchema,
+} from "@/types/FormTypes";
 
 const ReimbursementForm: React.FC = () => {
-	const [formState, setFormState] = useState<ReimbursementFormType>({
-		name: null,
-		phone: null,
-		address: null,
-		transactionDate: new Date(),
-		requesterType: "student",
-		uin: null,
-		deliveryMethod: "mailin",
-		purpose: null,
-		proof: null,
+	const form = useForm<ReimbursementFormType>({
+		initialValues: {
+			name: "",
+			phone: "",
+			address: "",
+			transactionDate: undefined,
+			requesterType: "student",
+			uin: "",
+			deliveryMethod: "mailin",
+			purpose: "",
+			proof: [],
+		},
+		validate: zodResolver(ReimbursementFormSchema),
 	});
 
-	const onFormChange = (key: string, val: any) => {
-		console.log("key:", key, "| val:", val);
-		setFormState((prevState) => ({
-			...prevState,
-			[key]: val,
-		}));
+	const handleSubmit = (values: ReimbursementFormType) => {
+		if (values.requesterType === "other") {
+			values.uin = "";
+			form.setFieldValue("uin", "");
+		}
+		console.log(values);
 	};
 
 	const filePill: FileInputProps["valueComponent"] = ({ value }) => {
-		const removePill = () => {
-			console.log("here");
+		const removePill = (pillIndex: number) => {
+			if (Array.isArray(value)) {
+				value.splice(pillIndex, 1);
+				form.setFieldValue("proof", value);
+			} else {
+				value = null;
+			}
+			console.log(form);
 		};
 
 		if (value === null) {
@@ -54,7 +68,12 @@ const ReimbursementForm: React.FC = () => {
 			return (
 				<PillGroup>
 					{value.map((file, index) => (
-						<Pill key={index} withRemoveButton onRemove={removePill}>
+						<Pill
+							key={index}
+							size="sm"
+							withRemoveButton
+							onRemove={() => removePill(index)}
+						>
 							{file.name}
 						</Pill>
 					))}
@@ -62,111 +81,122 @@ const ReimbursementForm: React.FC = () => {
 			);
 		}
 
-		return <Pill>{value.name}</Pill>;
+		return (
+			<Pill size="sm" withRemoveButton onRemove={() => removePill(0)}>
+				{value.name}
+			</Pill>
+		);
 	};
 
 	return (
 		<Container py={rem(20)}>
 			<Center>
-				<Stack justify="center">
-					<TextInput
-						label="Name"
-						description="Full name of the person who made the purchase."
-						aria-label="name"
-						placeholder="John Doe"
-						className={styles.textInput}
-						withAsterisk
-						onChange={(val) => onFormChange("name", val)}
-					/>
-					<TextInput
-						label="Phone Number"
-						description="Phone number of the person who made the purchase."
-						aria-label="phone number"
-						placeholder="123-456-7890"
-						className={styles.textInput}
-						withAsterisk
-						onChange={(val) => onFormChange("phone", val)}
-					/>
-					<TextInput
-						label="Mailing Address"
-						description="Valid non-P.O. Box address for check to to be sent (needed regardless of reimbursement method)."
-						aria-label="mailing address"
-						placeholder="1234 SASE TAMU Dr, College Station, TX 77840"
-						className={styles.textInput}
-						withAsterisk
-						onChange={(val) => onFormChange("address", val)}
-					/>
-					<DatePickerInput
-						label="Transaction Date"
-						description="The date the purchase was made."
-						aria-label="transaction date"
-						placeholder="YYYY-MM-DD"
-						valueFormat="YYYY-MM-DD"
-						className={styles.input}
-						leftSection={<IconCalendar className={styles.inputIcon} />}
-						leftSectionPointerEvents="none"
-						clearable
-						withAsterisk
-						onChange={(val) => onFormChange("transactionDate", val)}
-					/>
-					<SegmentedControl
-						value={formState.requesterType}
-						className={styles.input}
-						color={SASE_BLUE}
-						data={[
-							{ label: "TAMU Student", value: "student" },
-							{ label: "TAMU Employee", value: "employee" },
-							{ label: "Other", value: "other" },
-						]}
-						onChange={(val) => onFormChange("requesterType", val)}
-					/>
-					{formState.requesterType != "other" ? (
+				<form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+					<Stack justify="center">
 						<TextInput
-							label="UIN"
-							description="TAMU affiliated Universal Identification Number."
+							label="Full Name"
+							description="Full name of the person who made the purchase."
 							aria-label="name"
-							placeholder="123456789"
-							className={styles.textInput}
+							placeholder="John Doe"
+							className={`${styles.textInput} ${styles.inputMargin}`}
 							withAsterisk
-							onChange={(val) => onFormChange("uin", val)}
+							{...form.getInputProps("name")}
 						/>
-					) : null}
-					<SegmentedControl
-						value={formState.deliveryMethod}
-						className={styles.input}
-						color={SASE_BLUE}
-						data={[
-							{ label: "Mail In Check", value: "mailin" },
-							{ label: "SOFC Pickup", value: "pickup" },
-							{ label: "Direct Deposit", value: "deposit" },
-						]}
-						onChange={(val) => onFormChange("deliveryMethod", val)}
-					/>
-					<Textarea
-						label="Purpose of Purchase"
-						description="What was the purchase and how it is relevant to SASE."
-						aria-label="purpose of purchase"
-						placeholder="Forks and Spoons used for serving and eating food at company X GBM."
-						className={styles.textInput}
-						withAsterisk
-						autosize
-						minRows={3}
-						maxRows={5}
-						onChange={(val) => onFormChange("purpose", val)}
-					/>
-					<FileInput
-						label="Proof of Purchase"
-						aria-label="proof of purchase"
-						placeholder="HEB_Receipt-John_Doe.pdf"
-						leftSection={<IconFileTypePdf className={styles.inputIcon} />}
-						leftSectionPointerEvents="none"
-						valueComponent={filePill}
-						accept="pdf"
-						multiple
-						clearable
-						onChange={(val) => onFormChange("proof", val)}
-					/>
-				</Stack>
+						<TextInput
+							label="Phone Number"
+							description="Phone number of the person who made the purchase."
+							aria-label="phone number"
+							placeholder="1234567890"
+							className={`${styles.textInput} ${styles.inputMargin}`}
+							withAsterisk
+							{...form.getInputProps("phone")}
+						/>
+						<TextInput
+							label="Mailing Address"
+							description="Valid non-P.O. Box address for check to to be sent (needed regardless of reimbursement method)."
+							aria-label="mailing address"
+							placeholder="1234 SASE TAMU Dr, College Station, TX 77840"
+							className={`${styles.textInput} ${styles.inputMargin}`}
+							withAsterisk
+							{...form.getInputProps("address")}
+						/>
+						<DatePickerInput
+							label="Transaction Date"
+							description="The date the purchase was made."
+							aria-label="transaction date"
+							placeholder="YYYY-MM-DD"
+							valueFormat="YYYY-MM-DD"
+							maxDate={new Date()}
+							className={`${styles.input} ${styles.inputMargin}`}
+							leftSection={<IconCalendar className={styles.inputIcon} />}
+							leftSectionPointerEvents="none"
+							clearable
+							withAsterisk
+							{...form.getInputProps("transactionDate")}
+						/>
+						<SegmentedControl
+							value={form.values.requesterType}
+							className={`${styles.input} ${styles.inputMargin}`}
+							color={SASE_BLUE}
+							data={[
+								{ label: "TAMU Student", value: "student" },
+								{ label: "TAMU Employee", value: "employee" },
+								{ label: "Other", value: "other" },
+							]}
+							{...form.getInputProps("requesterType")}
+						/>
+						{form.values.requesterType != "other" ? (
+							<TextInput
+								label="UIN"
+								description="TAMU affiliated Universal Identification Number."
+								aria-label="name"
+								placeholder="123456789"
+								className={`${styles.textInput} ${styles.inputMargin}`}
+								withAsterisk
+								{...form.getInputProps("uin")}
+							/>
+						) : null}
+						<SegmentedControl
+							value={form.values.deliveryMethod}
+							className={`${styles.input} ${styles.inputMargin}`}
+							color={SASE_BLUE}
+							data={[
+								{ label: "Mail In Check", value: "mailin" },
+								{ label: "SOFC Pickup", value: "pickup" },
+								{ label: "Direct Deposit", value: "deposit" },
+							]}
+							{...form.getInputProps("deliveryMethod")}
+						/>
+						<Textarea
+							label="Purpose of Purchase"
+							description="What was the purchase and how it is relevant to SASE."
+							aria-label="purpose of purchase"
+							placeholder="Forks and Spoons used for serving and eating food at company X GBM."
+							className={`${styles.textInput} ${styles.inputMargin}`}
+							withAsterisk
+							autosize
+							minRows={3}
+							maxRows={5}
+							{...form.getInputProps("purpose")}
+						/>
+						<FileInput
+							label="Proof of Purchase"
+							aria-label="proof of purchase"
+							placeholder="HEB_Receipt-John_Doe.pdf"
+							className={`${styles.input} ${styles.inputMargin}`}
+							leftSection={<IconFileTypePdf className={styles.inputIcon} />}
+							leftSectionPointerEvents="none"
+							valueComponent={filePill}
+							accept="pdf"
+							multiple
+							clearable
+							{...form.getInputProps("proof")}
+						/>
+						<Button size="lg" type="submit" className={styles.fullButton}>
+							Submit
+						</Button>
+					</Stack>
+				</form>
 			</Center>
 		</Container>
 	);
--- src/types/FormTypes.ts
+++ src/types/FormTypes.ts
@@ -1,11 +1,50 @@
+import { z } from "zod";
+
 export interface ReimbursementFormType {
-	name: string | null;
-	phone: string | null;
-	address: string | null;
-	transactionDate: Date | null;
+	name: string;
+	phone: string;
+	address: string;
+	transactionDate: Date | undefined;
 	requesterType: "student" | " employee" | "other";
-	uin?: number | null;
+	uin?: string;
 	deliveryMethod: "mailin" | "pickup" | "deposit";
-	purpose: string | null;
-	proof: File[] | null;
+	purpose: string;
+	proof: File[];
 }
+
+export const ReimbursementFormSchema = z
+	.object({
+		name: z
+			.string()
+			.regex(/^[A-Za-z]+\s[A-Za-z]+$/, "First and last name is required")
+			.min(1, "Full name is required"),
+		phone: z
+			.string()
+			.regex(/^\d{10}/, "Phone number must be 10 digits")
+			.min(1, "Phone number is required"),
+		address: z.string().min(1, "Mailing address is required"),
+		transactionDate: z.date({ message: "Date of transaction is required" }),
+		requesterType: z.enum(["student", "employee", "other"]),
+		uin: z.string().optional(),
+		deliveryMethod: z.enum(["mailin", "pickup", "deposit"]),
+		purpose: z.string().min(1, "Purpose for purchase is required"),
+		proof: z
+			.array(
+				z.instanceof(File).refine((file) => file.type === "application/pdf", {
+					message: "File must be in PDF format",
+				})
+			)
+			.refine((array) => array.length > 0, {
+				message: "Proof of purchase (receipt and bank statement) is required",
+			}),
+	})
+	.refine(
+		(data) => {
+			console.log(data);
+			return data.requesterType !== "other" && data.uin && data.uin.length <= 0;
+		},
+		{
+			path: ["uin"],
+			message: "UIN is required for TAMU Students and Employees",
+		}
+	);
